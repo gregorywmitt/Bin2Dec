23f119143d63b2abdfc0594f45ddbb2784f4475f
GREGORY WILLIAM MITT COSTA <gregory.costa@brde.com.br>
2020-09-07 18:58:34 -0300

    commit

--- Bin2Dec.js
+++ Bin2Dec.js
@@ -1,7 +1,9 @@
 // App to convert binary number to decimal
+//alert("hello")
 function convert2Decimal(binImput){
     binImput = binImput.split('')
     binImput.forEach(element => {
         console.log(element)
     });
+convert2Decimal("10")
 }
