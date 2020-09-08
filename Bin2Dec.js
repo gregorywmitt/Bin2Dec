3715503030a97c9b8759770deede912c3b42cd35
GREGORY WILLIAM MITT COSTA <gregory.costa@brde.com.br>
2020-09-08 13:05:45 -0300

    commit

--- Bin2Dec.js
+++ Bin2Dec.js
@@ -3,10 +3,10 @@
 function convert2Decimal(binImput){
     console.log(binImput)
     binImput = binImput.split('')
+    binImput = binImput.reverse()
     console.log(binImput)
     binImput.forEach(element => {
         console.log(element)
     });
-convert2Decimal("10")
-console.log("teste console")
 }
+convert2Decimal("1010")
